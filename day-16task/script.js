b38c7fe72749d80adc301869e94aedda802f02a9
Graceyasnithiya <137611753+Graceyasnithiya@users.noreply.github.com>
2023-07-27 10:31:25 +0530

    Add files via upload

---
+++ day-16task/script.js
@@ -0,0 +1,59 @@
+console.log("Lets start the countdown");
+
+setTimeout(function(){
+
+    console.log("countdown 10");
+
+}, 1000);
+
+setTimeout(function () {
+
+    console.log("countdown 9");
+
+}, 2000);
+setTimeout(function(){
+
+  console.log("coundown 8"); 
+
+},3000);
+
+setTimeout(function(){
+
+    console.log("Countdown 7");
+}, 4000);
+
+setTimeout(function(){
+
+    console.log("countdown 6");
+}, 5000);
+
+setTimeout(function(){
+
+    console.log("Countdown 5");
+
+}, 6000);
+
+setTimeout(function(){
+
+    console.log("countdown 4");
+},7000);
+
+setTimeout(function(){
+
+    console.log("coutdown 3");
+}, 8000);
+
+setTimeout(function(){
+
+    console.log("countdown 2");
+},9000);
+
+setTimeout(function(){
+
+    console.log("countdown 1");
+},10000);
+
+setTimeout(function(){
+
+    console.log("HAPPY INDEPENDANCE DAY");
+},11000);
